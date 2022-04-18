2a9660bf81b234142b9583801aef2b4d39c85552
rhit-altschmn <altschmn@rose-hulman.edu>
2022-04-18 15:42:12 -0400

    first motor test

--- BotCode/test.py
+++ BotCode/test.py
@@ -1,5 +1,11 @@
-print("Hello World!!")
-print("Hi")
+#generic motor test
+
+from ev3dev2.auto import *
+from ev3dev2.motor import MediumMotor,OUTPUT_A
+
+m = MediumMotor(OUTPUT_A)
+
+m.run_timed(time_sp=3000, speed_sp=500)
 
 
 
