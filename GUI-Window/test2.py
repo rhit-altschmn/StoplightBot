94de2255ff5f95233300e27608e989c482657f11
rhit-hautid <hautid@rose-hulman.edu>
2022-04-29 16:15:26 -0400

    DONE

--- GUI-Window/test2.py
+++ GUI-Window/test2.py
@@ -1,12 +1,21 @@
 print("GUI window test")
 
 import tkinter
+from tkinter import ttk
 
 
 def main():
 
     root = tkinter.Tk()
     root["background"] = "light blue"
+
+    frame1 = ttk.Frame(root, padding=100)
+    frame1.grid()
+
+    on_button = ttk.Button(frame1, text="Turn ON")
+    on_button.grid()
+
+
     root.mainloop()
 
 main()
