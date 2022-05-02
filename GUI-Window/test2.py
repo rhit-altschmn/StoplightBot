fb91a81a693825f7a7da7ae97a410f7824b09d51
rhit-hautid <hautid@rose-hulman.edu>
2022-05-02 14:10:00 -0400

    DONE

--- GUI-Window/test2.py
+++ GUI-Window/test2.py
@@ -3,6 +3,17 @@ print("GUI window test")
 import tkinter
 from tkinter import ttk
 
+def print_word():
+    word = "You Lose"
+    print(word)
+
+def print_contents(entry_box):
+    contents_of_entry_box = entry_box.get()
+
+    if contents_of_entry_box == "Red":
+        print("You Lose")
+    else:
+        print("You Win")
 
 def main():
 
@@ -12,9 +23,19 @@ def main():
     frame1 = ttk.Frame(root, padding=100)
     frame1.grid()
 
-    on_button = ttk.Button(frame1, text="Turn ON")
+    on_button = ttk.Button(frame1, text="Level 1")
     on_button.grid()
 
+    print_stuff_button = ttk.Button(frame1, text="Level 2")
+    print_stuff_button["command"] = lambda: print_word()
+    print_stuff_button.grid()
+
+    my_entry_box = ttk.Entry(frame1)
+    my_entry_box.grid()
+
+    print_stuff_button = ttk.Button(frame1, text="New Button")
+    print_stuff_button["command"] = lambda: print_contents(my_entry_box)
+    print_stuff_button.grid()
 
     root.mainloop()
 
